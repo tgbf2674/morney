194dcae21b9111be3a72223d715aa648701c6938
tgbf2674 <tgbf2020@163.com>
2020-11-22 13:42:24 +0800

    创建layOut布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,9 @@
 <template>
-    <div class="nav-wrapper">
-        <div class="content">
-        Money.vue
+        <div>
+            <Layout>
+                <p>你好</p>
+            </Layout>
         </div>
-    <Nav />
-    </div>
 </template>
 
 <script lang="ts">
@@ -12,17 +11,3 @@
         name: "Money",
     };
 </script>
-
-<style lang="scss" scoped>
-    .nav-wrapper{
-        border: 1px solid green;
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-    }
-    .content{
-        border: 1px solid blue;
-        overflow: auto;
-        flex-grow: 1;
-    }
-</style>
