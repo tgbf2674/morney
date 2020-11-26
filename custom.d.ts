603b10e1f0463b1677bdf314048258b1f759a43c
tgbf2674 <tgbf2020@163.com>
2020-11-26 16:18:56 +0800

    使用全局tagList

--- custom.d.ts
+++ custom.d.ts
@@ -5,3 +5,22 @@ type RecordItem = {
     amount: number;
     createAt: Date | undefined;
 }
+
+type Tag = {
+    id: string;
+    name: string;
+}
+type TagListModel = {
+    data: Tag[];
+    fetch: () => Tag[];
+    create: (name: string) => 'success' | 'duplicated';
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+    save: () => void;
+    remove: (id: string) => boolean;
+
+}
+interface Window {
+
+    tagList: Tag[];
+
+}
